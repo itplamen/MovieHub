62626db88c361804b3a4465fa158bb549092ef25
itplamen <itplamen@gmail.com>
2024-07-19 00:44:59 +0300

    Created textFormatter

--- movie-hub-app/src/components/header/header.js
+++ movie-hub-app/src/components/header/header.js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import styles from "./header.module.css";
+import textFormatter from "@/utils/textFormatter";
 
 const Header = ({ text }) => {
   const router = useRouter();
@@ -13,7 +14,7 @@ const Header = ({ text }) => {
           <Breadcrumb>
             <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
             <Breadcrumb.Item active>
-              {currentPage.charAt(0).toUpperCase() + currentPage.slice(1)}
+              {textFormatter(currentPage)}
             </Breadcrumb.Item>
           </Breadcrumb>
         )}
