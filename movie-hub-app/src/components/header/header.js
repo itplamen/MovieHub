2849c2911f57c70307fb4dda50702a90c9fdee67
itplamen <itplamen@gmail.com>
2024-07-18 17:49:36 +0300

    Added breadcrumb to the header

--- movie-hub-app/src/components/header/header.js
+++ movie-hub-app/src/components/header/header.js
@@ -1,10 +1,25 @@
+import { useRouter } from "next/router";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
 import styles from "./header.module.css";
 
 const Header = ({ text }) => {
+  const router = useRouter();
+  const { type: currentPage } = router.query;
+
   return (
-    <div className={styles.description}>
-      <p>{text}</p>
-    </div>
+    <>
+      <div className={styles.description}>
+        {currentPage && (
+          <Breadcrumb>
+            <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+            <Breadcrumb.Item active>
+              {currentPage.charAt(0).toUpperCase() + currentPage.slice(1)}
+            </Breadcrumb.Item>
+          </Breadcrumb>
+        )}
+        <p>{text}</p>
+      </div>
+    </>
   );
 };
 
