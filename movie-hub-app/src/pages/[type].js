9a81d2bbb562a8c3164f519e0c2c3a96391578b4
itplamen <itplamen@gmail.com>
2024-07-19 00:45:37 +0300

    Fixed bug with global styles and meta tags

--- movie-hub-app/src/pages/[type].js
+++ movie-hub-app/src/pages/[type].js
@@ -1,13 +1,35 @@
 import { useRouter } from "next/router";
+import Head from "next/head";
 import config from "@/data/configurations.json";
 import MovieCardList from "@/components/movieCard/movieCardList";
+import textFormatter from "@/utils/textFormatter";
 
 const Trending = () => {
   const router = useRouter();
   const { type } = router.query;
   const trending = config.trendings.find((obj) => obj.type === type);
 
-  return <>{trending && <MovieCardList {...trending} />}</>;
+  return (
+    <>
+      {trending && (
+        <>
+          <Head>
+            <title>{textFormatter(type)}</title>
+            <meta
+              name="description"
+              content={`Trending ${textFormatter(type)}`}
+            />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <MovieCardList {...trending} />
+        </>
+      )}
+    </>
+  );
 };
 
 export default Trending;
